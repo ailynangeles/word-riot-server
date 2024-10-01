b7e0dceb4fc1eba7b06e5209fc015a4f6c7cb6d8
rooksFX <ron_nuqui@yahoo.com>
2024-10-01 17:58:34 +0800

    Switch to Socket io

---
+++ src/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { Server: IOServer } = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new IOServer(server, {
+  path: "/socket",  // Set the path explicitly
+  // transports: ['polling', 'websocket'],
+});
+
+const games = {};
+
+io.on('connection', (socket) => {
+  console.log('New player connected:', socket.id);
+
+  socket.on('connect_error', (err) => {
+    console.error('Connection error:', err);
+  });
+
+  // Create a game room
+  socket.on('createGame', (gameCode, isSynonym) => {
+    socket.join(gameCode);
+    games[gameCode] = { gameCode, player1: isSynonym, player2: !isSynonym };
+    console.log(`Game room ${gameCode} created by ${socket.id}`);
+    socket.emit('gameCreated', gameCode);
+  });
+
+  // Join an existing game room
+  socket.on('joinGame', (gameCode) => {
+    const room = io.sockets.adapter.rooms.get(gameCode);
+    if (room && room.size === 1) {
+      socket.join(gameCode);
+      console.log(`${socket.id} joined game room ${gameCode}`);
+      socket.emit('gameJoined', gameCode, games[gameCode].player2);
+      io.to(gameCode).emit('startGame');  // Notify both players to start the game
+    } else {
+      socket.emit('error', 'Game room not available or already full');
+    }
+  });
+
+  socket.on('opponentAction', (gameCode, data, playerNumber) => {
+    socket.to(gameCode).emit('opponentAction', data, playerNumber);
+  });
+
+  socket.on('leave', (gameCode) => {
+    console.log("Player left > socket.id:", socket.id);
+    socket.leave(gameCode);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Player disconnected:', socket.id);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 9090;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
